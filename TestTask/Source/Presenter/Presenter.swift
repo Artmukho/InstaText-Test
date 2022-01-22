8246d4ed9c2c1b69614ec576e0449c026c1fbc9a
Artem Muho <artem.muho@yandex.ru>
2022-01-23 00:45:06 +0300

    Realization presenter by ViewOutput protocol.

--- TestTask/Source/Presenter/Presenter.swift
+++ TestTask/Source/Presenter/Presenter.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+
+class Presenter: ViewOutput {
+    
+    var view: ViewInput?
+    
+    func saveButtonAction() {
+        view?.selectDisabled()
+        view?.takePicture()
+        view?.editEnabled()
+    }
+}
